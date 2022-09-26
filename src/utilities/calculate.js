90cc0395b566a066dcf57aa56c48eb32e93000fd
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-09-26 11:07:38 +0600

    reducer

--- src/utilities/calculate.js
+++ src/utilities/calculate.js
@@ -8,7 +8,14 @@ const  multiply = (first,second) =>{
 }
 
 
+const numbers = [20,24,235,65,89];
+const sumReducer = (previous,current) => previous + current;
+const total = numbers.reduce(sumReducer,0);
 
+
+const items = [
+    {}
+]
 
 
  export {add, multiply}
